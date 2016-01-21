75b85f4533eeb2c3c14b73b82fbafd6a0fc27dd8
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-01-20 23:47:50 -0800

    bar graph now displays words

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,38 +2,18 @@ var words = $('#top-tweets').text().split(",");
 var nums = $('#top-nums').text().split(",");
 var data = [];
 for (var i in nums) {
-  data.push(parseInt(nums[i]));
+  var obj = { "name": words[i], "value": parseInt(nums[i]) }
+  data.push(obj);
 }
 
 d3.select("#tweet-graph")
   .selectAll("div")
     .data(data)
   .enter().append("div")
-    .style("width", function(d) { return d * 60 + "px"; })
-    .text(function(d) { return d; });
+    .style("width", function(d) { return d.value * 80 + "px"; })
+    .text(function(d) { return d.name + ": " + d.value; });
 
-// var width = 420,
-//     barHeight = 20;
-//
-// var x = d3.scale.linear()
-//     .domain([0, d3.max(data)])
-//     .range([0, width]);
-//
-// var chart = d3.select("#tweet-graph")
-//     .attr("width", width)
-//     .attr("height", barHeight * data.length);
-//
-// var bar = chart.selectAll("g")
-//     .data(data)
-//     .enter().append("g")
-//     .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-//
-// bar.append("rect")
-//     .attr("width", x)
-//     .attr("height", barHeight - 1);
-//
-// bar.append("text")
-//     .attr("x", function(d) { return x(d) - 3; })
-//     .attr("y", barHeight / 2)
-//     .attr("dy", ".35em")
-//     .text(function(d) { return d; });
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
+}
