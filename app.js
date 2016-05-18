a9ff27d747db17d8ec8f46a020346698f4d61111
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-05-17 22:33:27 -0700

    add gulp

--- app.js
+++ app.js
@@ -58,4 +58,19 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+});
+
 module.exports = app;
