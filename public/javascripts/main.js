18f6b1afcfeff2e05bb5b52988b69f31ad9ff24e
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-05-18 22:38:35 -0700

    the simplest react updates

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,19 +1,28 @@
-var words = $('#top-tweets').text().split(",");
-var nums = $('#top-nums').text().split(",");
-var data = [];
-for (var i in nums) {
-  var obj = { "name": words[i], "value": parseInt(nums[i]) }
-  data.push(obj);
-}
+// var words = $('#top-tweets').text().split(",");
+// var nums = $('#top-nums').text().split(",");
+// var data = [];
+// for (var i in nums) {
+//   var obj = { "name": words[i], "value": parseInt(nums[i]) }
+//   data.push(obj);
+// }
+//
+// d3.select("#tweet-graph")
+//   .selectAll("div")
+//     .data(data)
+//   .enter().append("div")
+//     .style("width", function(d) { return d.value * 60 + "px"; })
+//     .text(function(d) { return d.name + ": " + d.value; });
+//
+// function type(d) {
+//   d.value = +d.value; // coerce to number
+//   return d;
+// }
 
-d3.select("#tweet-graph")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return d.value * 60 + "px"; })
-    .text(function(d) { return d.name + ": " + d.value; });
-
-function type(d) {
-  d.value = +d.value; // coerce to number
-  return d;
-}
+ReactDOM.render(
+  <h1>twitterology</h1>,
+  document.getElementById('title')
+)
+ReactDOM.render(
+  <p>find out what's in your twitter cloud, you twitter obsessed scoundrel</p>,
+  document.getElementById('opening-words')
+);
